b7e88034dfc111a6292436a1275aaa7b81b55ec5
Chaspen <chaspen27@gmail.com>
2026-07-21 20:54:12 -0400

    overhaul

---
+++ js/lastfm.js
@@ -0,0 +1,26 @@
+async function LastPlayed(onfetch) {
+    
+    const url = `https://lastfm-last-played.biancarosa.com.br/Chaspen/latest-song`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        // console.log(json);
+        // console.log(json.track.name)
+        // console.log(json.track.artist['#text'])
+        document.getElementById("cover").src = json.track.image[2]['#text']
+        document.getElementById("artist").innerHTML = json.track.artist['#text'];
+        document.getElementById("songtitle").innerHTML = json.track.name;
+        document.getElementById("nowplaying").onclick = function(){ window.open(json.track.url) };
+        
+
+        onfetch();
+    } catch (error) {
+        console.error(error.message);     
+    }
+}
+
+LastPlayed();
