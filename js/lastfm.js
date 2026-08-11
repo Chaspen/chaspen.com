db89820f4abd11bfb829c21fdb972a01f2de5d2d
Chaspen <chaspen27@gmail.com>
2026-08-11 19:33:32 -0400

    add more shit!!!

--- js/lastfm.js
+++ js/lastfm.js
@@ -12,9 +12,12 @@ async function LastPlayed(onfetch) {
         // console.log(json.track.name)
         // console.log(json.track.artist['#text'])
         document.getElementById("cover").src = json.track.image[2]['#text']
+        if (json.track.image[2]['#text'] == "") {
+            document.getElementById("cover").src = "/img/cover-not-found.png"
+        }
         document.getElementById("artist").innerHTML = json.track.artist['#text'];
         document.getElementById("songtitle").innerHTML = json.track.name;
-        document.getElementById("nowplaying").onclick = function(){ window.open(json.track.url) };
+        document.getElementById("nowplaying").onclick = function(){ window.open(json.track.url)};
         
 
         onfetch();
